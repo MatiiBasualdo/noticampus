8dac702bf2698c4ef6db7be29781a08faaaf1bbd
ulises lopez <uliseslopez1112@gmail.com>
2017-06-16 11:03:03 -0300

    Agregando noticias

--- noticampus/views.py
+++ noticampus/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Noticia
+
 
 # Create your views here.
 
 def inicio(request):
-	return render(request, 'inicio.html',{})
+	noticias = Noticia.objects.all().order_by('created_date')
+	return render(request, 'inicio.html', {'noticias': noticias})
+
+
+
+
